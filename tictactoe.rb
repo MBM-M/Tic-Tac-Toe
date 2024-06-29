0f5088f21bd2377bb98be423a137bf1bd51a9ecb
Muhammad <muhammadbm.01@gmail.com>
2024-06-28 20:33:12 -0400

    tictactoe.rb

--- tictactoe.rb
+++ tictactoe.rb
@@ -8,15 +8,16 @@ class TicTacToe
 
   def play
     while !@@win 
+      @round = @@turn%2
       if @@turn%2 == 0
         puts "Player 1, please select a placement: "
         box = gets.chomp.to_i
-        player1_move(box)
+        player_move(box, @round)
         tie?
       elsif @@turn%2 == 1
         puts "Player 2, please select a placement: "
         box = gets.chomp.to_i
-        player2_move(box)
+        player_move(box, @round)
         tie?
       end
     end
@@ -24,17 +25,16 @@ class TicTacToe
 
   private
 
-  def player1_move(box)
+  def player_move(box, player)
     if box < 1 || box > 9
       puts "Out of range. Try another placement"
       return
-    end
-    if @@grid[box-1] == "X" || @@grid[box-1] == "O"
+    elsif @@grid[box-1] == "X" || @@grid[box-1] == "O"
       puts "The box has already been taken. Try another placement..."
       return
     end
     @@turn += 1
-    @@grid[box-1] = "X"#Player1.SYMBOL
+    @@turn%2 == 0? @@grid[box-1] = "O": @@grid[box-1] = "X"
     @@grid = @@grid[0..2] + @@grid[3..5] + @@grid[6..8]
     #p @@grid
     puts @@grid[0].to_s + "|" + @@grid[1].to_s + "|" + @@grid[2].to_s
@@ -42,40 +42,11 @@ class TicTacToe
     puts @@grid[3].to_s + "|" + @@grid[4].to_s + "|" + @@grid[5].to_s
     puts "-----"
     puts @@grid[6].to_s + "|" + @@grid[7].to_s + "|" + @@grid[8].to_s
-    @@player1_seq.push(box).sort!
-    #puts @@player1_seq
+    @player = @@turn%2 == 0? @@player1_seq.push(box).sort!: @@player2_seq.push(box).sort!
+    @winner = @@turn%2 == 0? "2": "1"
     @@WINNING_SEQUENCE.each do |iter|
-      if iter.all? {|pos| @@player1_seq.include? (pos)}
-        puts "Player 1 has won"
-        @@win = true
-        break
-      end
-    end
-  end
-
-  def player2_move(box)
-    if box < 1 || box > 9
-      puts "Out of range. Try another placement"
-      return
-    end
-    if @@grid[box-1] == "X" || @@grid[box-1] == "O"
-      puts "The box has already been taken. Try another placement..."
-      return
-    end
-    @@turn += 1
-    @@grid[box-1] = "O"#Player1.SYMBOL
-    @@grid = @@grid[0..2] + @@grid[3..5] + @@grid[6..8]
-    #p @@grid
-    puts @@grid[0].to_s + "|" + @@grid[1].to_s + "|" + @@grid[2].to_s
-    puts "-----"
-    puts @@grid[3].to_s + "|" + @@grid[4].to_s + "|" + @@grid[5].to_s
-    puts "-----"
-    puts @@grid[6].to_s + "|" + @@grid[7].to_s + "|" + @@grid[8].to_s
-    @@player2_seq.push(box).sort!
-    #puts @@player2_seq
-    @@WINNING_SEQUENCE.each do |iter|
-      if iter.all? {|pos| @@player2_seq.include? (pos)}
-        puts "Player 2 has won"
+      if iter.all? {|pos| @player.include? (pos)}
+        puts "Player #{@winner} has won!"
         @@win = true
         break
       end
@@ -83,12 +54,11 @@ class TicTacToe
   end
 
   def tie?
-    if @@turn > 8
+    if @@turn > 8 && @@win == false
       puts "Its a tie."
       @@win = true
     end
   end
-
 end
 
 game = TicTacToe.new
